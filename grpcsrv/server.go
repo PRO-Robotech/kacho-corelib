c2bff5f3779bc0b5d74dedcddd77db69064e41c1
Kacho Workspace <workspace@kacho.dev>
2026-05-03 19:24:01 +0300

    fix: goose StatementBegin для PL/pgSQL function + grpc reflection в NewServer

--- grpcsrv/server.go
+++ grpcsrv/server.go
@@ -4,13 +4,16 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
-// NewServer создаёт gRPC-сервер с зарегистрированным Health-сервисом в состоянии SERVING.
+// NewServer создаёт gRPC-сервер с зарегистрированными Health-сервисом в состоянии SERVING
+// и server-reflection (для grpcurl, debug, dev-tooling).
 func NewServer(opts ...grpc.ServerOption) *grpc.Server {
 	s := grpc.NewServer(opts...)
 	h := health.NewServer()
 	h.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
 	healthpb.RegisterHealthServer(s, h)
+	reflection.Register(s)
 	return s
 }
--- migrations/common/0001_resource_events.sql
+++ migrations/common/0001_resource_events.sql
@@ -15,15 +15,19 @@ CREATE INDEX resource_events_kind_rv_idx ON resource_events (resource_kind, reso
 CREATE INDEX resource_events_cleanup_idx ON resource_events (created_at);
 
 -- Вспомогательная функция для ресурсных таблиц: поднимает resource_version при UPDATE.
+-- +goose StatementBegin
 CREATE OR REPLACE FUNCTION bump_resource_version() RETURNS TRIGGER AS $$
 BEGIN
   NEW.resource_version := nextval('resource_version_seq');
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+-- +goose StatementEnd
 
 -- +goose Down
 
+-- +goose StatementBegin
 DROP FUNCTION IF EXISTS bump_resource_version();
+-- +goose StatementEnd
 DROP TABLE IF EXISTS resource_events;
 DROP SEQUENCE IF EXISTS resource_version_seq;
